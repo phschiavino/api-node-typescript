7e078242b360ea5cf249b4d0667fff4d861d120c
dbc.pedro <dbc.pedro@unimedpoa.com.br>
2023-04-19 10:26:31 -0300

    feat: controllers and database refresh

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import {server} from './server/server';
+import { Knex } from './server/database/knex';
+import { server } from './server/Server';
 
-server.listen(3333, () => console.log('API rodando!'));
+
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
+
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  console.log('Rodando migrations');
+
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed.run()
+        .then(() => startServer())
+        .catch(console.log);
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
---
+++ src/server/controllers/usuarios/SignIn.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+
+import { UsuariosProvider } from '../../database/providers/usuarios';
+import { validation } from '../../shared/middleware';
+import { IUsuario } from '../../database/models';
+import { PasswordCrypto } from '../../shared/services';
+
+
+interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> { }
+
+export const signInValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(yup.object().shape({
+    senha: yup.string().required().min(6),
+    email: yup.string().required().email().min(5),
+  })),
+}));
+
+export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
+  const { email, senha } = req.body;
+
+
+  const result = await UsuariosProvider.getByEmail(email);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      errors: {
+        default: 'Email ou senha são inválidos'
+      }
+    });
+  }
+
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+  if (!passwordMatch) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      errors: {
+        default: 'Email ou senha são inválidos'
+      }
+    });
+  } else {
+    return res.status(StatusCodes.OK).json({ accessToken: 'teste.teste.teste' });
+  }
+};
